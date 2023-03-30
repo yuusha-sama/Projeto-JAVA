fd2617dec800a39cf036ea170814e1193f28deb0
Gabriel Lima <49318475+yuusha-sama@users.noreply.github.com>
2023-03-29 21:41:08 -0300

    Add files via upload

--- alura-stikers/src/App.java
+++ alura-stikers/src/App.java
@@ -1,11 +1,6 @@
 import java.io.File;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
@@ -14,38 +9,47 @@ public class App {
         
         
         // fazer uma conecção HTTP, e buscar os TOP 250 filmes
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json"; // usando endereço alternativo
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient(); 
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
         
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json"; // usando endereço alternativo
+        ExtratorDeConteudoDoIMDB extrator = new ExtratorDeConteudoDoIMDB();
 
+        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/NASA-APOD.json";
+        //ExtratorDeConteudoDaNasa extrator = new ExtratorDeConteudoDaNasa();
+
+        //URI endereco = URI.create(url);
+        //var client = HttpClient.newHttpClient(); 
+        //var request = HttpRequest.newBuilder(endereco).GET().build();
+        //HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        //String body = response.body() ;
+        
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url); 
 
         // extrair so os dados que interessam (titulo, poster, classificação)
         var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        List<Map<String, String>> listaDeConteudo = parser.parse(json);
         
 
         // exibir dados
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
         var geradora = new GeradorDeFigurinhaJava();
-
-        var diretorio = new File("figurinha");
-        diretorio.mkdir();
+        //var diretorio = new File("figurinha");
+        //diretorio.mkdir();
         
-        for (Map<String,String> filme : listaDeFilmes) {
 
-            String urlImage = filme.get("image");
-            String titulo = filme.get("title");
+        for (int i = 0; i < 3; i++) {
 
-            InputStream inputStream = new URL(urlImage).openStream();
-            String nomeArquivo = "figurinha/" + titulo + ".png";
+            Conteudo conteudo = conteudos.get(i);
 
-   
+           
+
+            InputStream inputStream = new URL(conteudo.getUrlImage()).openStream();
+            String nomeArquivo = "figurinha/" + conteudo.getTitulo() + ".png";
+            
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println(filme.get("title"));
+            System.out.println(conteudo.getTitulo());
             System.out.println();
         }
 
